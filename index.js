f54fd5350f3bcbe3d31ea41e804d2ce2c7f2626a
dbrugne <damien.brugne@gmail.com>
2015-08-24 13:46:30 +0200

    Minor

--- index.js
+++ index.js
@@ -74,11 +74,14 @@ function donutCommonCode(_, linkify) {
      *
      ******************************************************************/
 
-    cloudinarySize: function(url, size) {
+    cloudinarySize: function(url, size, crop) {
       if (!url || url === '')
         return null;
 
       size = size || 100;
+
+      if (crop)
+        url = url.replace(/__crop__/, crop);
 
       return url
               .replace(/__width__/, size)
